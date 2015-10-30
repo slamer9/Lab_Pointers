1a1787b34cebdfec203a46340d896e6b272e2f13
unknown <reevedun000@779CTE220LAB05.alpine.local>
2015-10-30 10:04:25 -0600

    Initialization

---
+++ Lab_pointers/Lab_pointers.cpp
@@ -0,0 +1,16 @@
+/* Pointers_lab.cpp : main project file.
+* Duncan Reeves A-2
+*
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
